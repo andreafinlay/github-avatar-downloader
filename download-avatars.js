ce0d4c2d66440f81374bcc40fb787c563ea9154d
Andrea <finlayandi@gmail.com>
2018-04-16 23:04:24 +0000

    adds authorization

--- download-avatars.js
+++ download-avatars.js
@@ -1,8 +1,20 @@
 var request = require('request');
+var secret = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, callback) {
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'user-agent': 'request',
+      'authorization': secret
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
