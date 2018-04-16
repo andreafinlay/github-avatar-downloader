8099c18e8f2af5eb0da21733bc7c3880988b2ad0
Andrea <finlayandi@gmail.com>
2018-04-16 22:36:49 +0000

    adds function and callback

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,11 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, callback) {
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
